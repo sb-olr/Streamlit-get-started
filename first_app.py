868cf6f88320ab2042225e7cfa0498c565095745
Sujit Badle <sujit.badle@nowtilus.tv>
2021-05-12 11:43:54 +0200

    demo progress bar

--- first_app.py
+++ first_app.py
@@ -1,3 +1,4 @@
+# https://docs.streamlit.io/en/stable/getting_started.html
 import streamlit as st
 # To make things easier later, we're also importing numpy and pandas for
 # working with sample data.
@@ -70,3 +71,22 @@ if pressed:
 
 expander = st.beta_expander("FAQ")
 expander.write("Here you could put in some really, really long explanations...")
+
+
+# Show progress
+
+import time
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
